0263fcc316b809da51b7d1396eb28f00f1570d6a
omadruga <omadruga@gmail.com>
2024-02-14 10:25:02 -0300

    separando recorrencias por eternas e parceladas

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,5 @@
 <template>
   <div class="flex">
-    <div class="w-1/5 bg-slate-900 p-4">Nada por aqui ainda...</div>
-
     <div class="w-2/3 p-4 shadow-md">
       <div v-for="transaction in transactions">
         <div
@@ -88,7 +86,7 @@
       </div>
     </div>
 
-    <div class="w-1/4 bg-slate-900 p-4">
+    <div class="w-1/3 bg-slate-900 p-4">
       <div v-if="loggedIn">
         <UDivider label="AÇÕES" class="pb-4" />
         <CrudAddButton
@@ -110,9 +108,63 @@
 
       <UDivider label="RECORRÊNCIAS DO MÊS" class="py-4" />
       <div v-for="transaction in transactions">
+        <UDivider label="Recorrências Eternas" class="py-4" />
         <div
           v-if="
             transaction.reacurring &&
+            transaction.parcels == -1 &&
+            $dayjs(transaction.date).isAfter(
+              $dayjs().startOf('month').subtract(1, 'day')
+            )
+          "
+        >
+          <div class="flex justify-start">
+            <!-- date -->
+            <div class="text-center text-sm self-end min-w-fit pr-4">
+              <span class="text-gray-400">
+                {{ $dayjs(transaction.date).add(1, "day").format("DD") }}
+              </span>
+            </div>
+
+            <!-- description -->
+            <div class="w-96">
+              <span class="text-sm leading-5 block text-gray-400"
+                >{{ transaction.description }}
+                <UBadge v-if="transaction.reacurring" variant="soft" size="xs">
+                  <span v-if="transaction.parcels > 0">
+                    {{ transaction.currentParcel }} / {{ transaction.parcels }}
+                  </span>
+                  <span v-else>
+                    <UIcon name="i-heroicons-outline-refresh" dynamic />
+                  </span>
+                </UBadge>
+              </span>
+            </div>
+
+            <!-- amount -->
+            <div
+              class="flex self-center text-end ml-auto text-sm text-gray-400"
+            >
+              {{ transaction.amount.toFixed(2) }}
+            </div>
+          </div>
+
+          <UMeter
+            v-if="transaction.parcels > 0"
+            size="2xs"
+            :indicator="false"
+            color="teal"
+            :value="`${
+              (transaction.currentParcel / transaction.parcels) * 100
+            }`"
+          />
+        </div>
+
+        <UDivider label="Recorrências Parceladas" class="py-4" />
+        <div
+          v-if="
+            transaction.reacurring &&
+            transacation.parcels >= 0 &&
             $dayjs(transaction.date).isAfter(
               $dayjs().startOf('month').subtract(1, 'day')
             )
