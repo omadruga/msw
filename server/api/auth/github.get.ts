db0348c305a487438a3b0f0581e47de50f313948
omadruga <omadruga@gmail.com>
2024-02-14 10:18:11 -0300

    allow only me to login

--- server/api/auth/github.get.ts
+++ server/api/auth/github.get.ts
@@ -1,7 +1,11 @@
 export default oauth.githubEventHandler({
   async onSuccess(event, { user }) {
-    console.log(user);
-    await setUserSession(event, { user });
+    // hack only allow myself to login xD
+    if (user?.html_url == "https://github.com/omadruga") {
+      await setUserSession(event, { user });
+    } else {
+      await clearUserSession(event);
+    }
     return sendRedirect(event, "/");
   },
 });
